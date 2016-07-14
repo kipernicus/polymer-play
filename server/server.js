a7795ac46b03c52e137d02ac0db6da2896c883a2
kipernicus <kip.armstrong@gmail.com>
2016-07-14 15:09:46 -0600

    Initial page

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ var express = require('express');
 
 var app = express();
 
+app.use(express.static('client'));
 app.listen(3010, function() {
   console.log('Polymer Play listening on port 3010!');
 });
